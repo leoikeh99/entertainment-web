cf2617633dd3a3dac1c268759d0d229b99c8f7f5
Ikeh Leonard <leoikeh99@gmail.com>
2022-10-25 14:53:43 +0100

    Refactored user api route

--- pages/api/auth/user.ts
+++ pages/api/auth/user.ts
@@ -1,23 +1,28 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { BASE_URL } from "../../../config";
-import axios from "axios";
 import authenticate from "../../../middleware/authenticate";
+import clientPromise from "../../../lib/mongodb";
+import { ObjectId } from "mongodb";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { id } = req.body;
 
-  await axios
-    .get(`${BASE_URL}/_users/${id}`)
-    .then(async (val) => {
-      const res2 = await axios.get(`${BASE_URL}/_bookmarks?userId=${id}`);
-      res.status(200).json({
-        user: { email: val.data.email, id: val.data.id },
-        bookmarks: res2.data,
-      });
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
+  const client = await clientPromise;
+  const db = client.db("myFirstDatabase");
+
+  const user = await db.collection("_users").findOne(new ObjectId(id));
+  if (!user) {
+    return res.status(403).json({ message: "Unauthorized" });
+  }
+
+  const bookmarks = await db
+    .collection("bookmarks")
+    .find({ user: user._id })
+    .toArray();
+
+  res.status(200).json({
+    user: { email: user.email, id: user._id.toString() },
+    bookmarks,
+  });
 };
 
 export default authenticate(handler, "GET");
